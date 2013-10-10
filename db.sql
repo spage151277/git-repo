616ccc839d80a3d126df5d8b66165d2f78f92bcb
Swanand P <spage151277@gmail.com>
2013-10-10 10:48:29 +0800

    Update db.sql

--- db.sql
+++ db.sql
@@ -1556,4 +1556,19 @@ CREATE DIRECTORY datadir1 AS  '/usr/oracle/app/product/11.2.0/dbhome_1/load_db';
 select directory_path from dba_directories; 
 
 
+Estimating the Buffer Cache ( db_cache_size)
+
+COLUMN size_for_estimate          FORMAT 999,999,999,999 heading 'Cache Size (MB)'
+COLUMN buffers_for_estimate       FORMAT 999,999,999 heading 'Buffers'
+COLUMN estd_physical_read_factor  FORMAT 999.90 heading 'Estd Phys|Read Factor'
+COLUMN estd_physical_reads        FORMAT 999,999,999 heading 'Estd Phys| Reads'
+
+SELECT size_for_estimate, buffers_for_estimate, estd_physical_read_factor, estd_physical_reads
+   FROM V$DB_CACHE_ADVICE
+   WHERE name          = 'DEFAULT'
+     AND block_size    = (SELECT value FROM V$PARAMETER WHERE name = 'db_block_size')
+     AND advice_status = 'ON';
+     
+     
+
 
